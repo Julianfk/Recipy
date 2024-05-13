5e349264476e7d1dfd6f38e6960f2ba782c87738
Julian <julianfk05@gmail.com>
2024-05-13 15:20:33 +0100

    Finished Gatsby Tutorial Part 6

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,42 @@
 import * as React from 'react'
-import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
-import Seo from '../components/seo'
-const IndexPage = () => {
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
+
+const BlogPage = ({ data }) => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage
-        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
-        src="../images/mcfatty.jpg"
-      />
+    <Layout pageTitle="My Blog Posts">
+      {
+        data.allMdx.nodes.map(node => (
+          <article key={node.id}>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p>Posted: {node.frontmatter.date}</p>
+          </article>
+        ))
+      }
     </Layout>
   )
 }
 
+export const query = graphql`
+  query {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+          slug
+        }
+        id
+      }
+    }
+  }
+`
 
-export const Head = () => <Seo title="Home Page" />
+export const Head = () => <Seo title="My Blog Posts" />
 
-export default IndexPage
+export default BlogPage
