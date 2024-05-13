c16e35106ae248c2628c601d5917042bd69a29ea
Julian <julianfk05@gmail.com>
2024-05-13 14:52:37 +0100

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,21 @@
  */
 module.exports = {
   siteMetadata: {
+    title: "My First Gatsby Site",
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [    "gatsby-plugin-image",
-  "gatsby-plugin-sharp",],
+  
+  plugins: [    
+  "gatsby-plugin-image",
+  "gatsby-plugin-sharp", {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: `blog`,
+      path: `${__dirname}/blog`,
+    }
+  }, 
+  "gatsby-plugin-mdx",
+
+],
+  
 }
